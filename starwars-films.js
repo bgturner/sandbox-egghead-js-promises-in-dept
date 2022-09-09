4d749918ffbcd0809ce3d308ff057f671a219a86
Benjamin Turner <ben.turner@wpengine.com>
2022-09-09 10:24:35 -0700

    refactor filmTitles logic to function to make fullfillment handlers cleaner

--- starwars-films.js
+++ starwars-films.js
@@ -8,8 +8,11 @@ console.log(filmTitles);
 fetch(API_URL + "films")
   .then((response) => response.json())
   .then((films) => {
-    const filmTitles = films
-      .sort((a, b) => a.episode_id - b.episode_id)
-      .map((film) => `${film.episode_id}. ${film.title}`);
-    console.log(filmTitles);
+    console.log(getFilmTitles(films));
   });
+
+function getFilmTitles(films) {
+  return films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map((film) => `${film.episode_id}. ${film.title}`);
+}
