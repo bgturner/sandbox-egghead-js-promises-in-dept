c945b1397413cdbd9d841c839292250a25b1db52
Benjamin Turner <ben.turner@wpengine.com>
2022-09-09 10:19:08 -0700

    mimic a loading state
    The video gets a dom element on a page, but we're doing in via node.

--- starwars-films.js
+++ starwars-films.js
@@ -4,6 +4,9 @@ const API_URL = "https://starwars.egghead.training/";
 
 const responsePromise = fetch(API_URL + "films");
 
+let filmTitles = "Loading...";
+console.log(filmTitles);
+
 responsePromise
   .then((response) => {
     return response.json();
@@ -12,6 +15,5 @@ responsePromise
     const filmTitles = films
       .sort((a, b) => a.episode_id - b.episode_id)
       .map((film) => `${film.episode_id}. ${film.title}`);
-    console.log("filmTitles: ", filmTitles);
-    console.log("films: ", films);
+    console.log(filmTitles);
   });
